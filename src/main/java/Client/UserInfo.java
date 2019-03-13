1a92d86989e58b17c76ee42b0c9fec7368ea06e4
May <May_i21@163.com>
2019-03-13 08:45:40 +0800

    第三周周三早上的提交,添加了一些按钮，加入许多功能

---
+++ src/main/java/Client/UserInfo.java
@@ -0,0 +1,44 @@
+package Client;
+
+import util.MySqlDao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UserInfo {
+	public static String getId(String username){
+		String id = null;
+		Connection connection = MySqlDao.getConnection();
+		ResultSet resultSet = null;
+		Statement statement = null;
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT id from wechat where userName=" + username);
+			while (resultSet.next()){
+				id = resultSet.getString("id");
+			}
+
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return id;
+	}
+	public static String getUserName(String id){
+		String userName = null;
+		Connection connection = MySqlDao.getConnection();
+		ResultSet resultSet = null;
+		Statement statement = null;
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT userName from wechat where id=" + id);
+			while (resultSet.next()){
+				userName = resultSet.getString("userName");
+			}
+
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return userName;
+	}
+}
