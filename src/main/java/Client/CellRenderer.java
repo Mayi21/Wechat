45eeb7aa522218fe44a2de18e58daffddc74a0f3
xaohii <xaohii@163.com>
2022-11-18 21:28:16 +0800

    update

--- src/main/java/Client/CellRenderer.java
+++ src/main/java/Client/CellRenderer.java
@@ -3,17 +3,17 @@ package Client;
 import javafx.geometry.Pos;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
 
+/**
+ * 这个貌似是用户列表
+ * */
 class CellRenderer implements Callback<ListView<String>,ListCell<String>>{
     @Override
     public ListCell<String> call(ListView<String> p) {
         ListCell<String> cell = new ListCell<String>(){
-
             @Override
             protected void updateItem(String user, boolean bln) {
                 super.updateItem(user, bln);
