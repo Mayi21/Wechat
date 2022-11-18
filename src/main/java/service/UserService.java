eff9fdc51b5b20526c4b639402eccb16a4d269a4
xaohii <xaohii@163.com>
2022-11-18 21:29:11 +0800

    update

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -7,27 +7,38 @@ import mapper.UserMapper;
 import org.apache.ibatis.session.SqlSession;
 import util.MyBatisUtil;
 
+import java.lang.ref.PhantomReference;
+
 public class UserService {
-	public static void main(String[] args) {
-		SqlSession sqlSession = MyBatisUtil.getSqlSession();
-//		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-//		UserDo user = userMapper.getUserByUserName("xiaoli");
-//		if (Objects.isNull(user)) {
-//			System.out.println("查无此人");
-//		} else {
-//			System.out.println(user.toString());
-//		}
 
-		UserFriendMapper mapper = sqlSession.getMapper(UserFriendMapper.class);
-		mapper.addFriend(new UserFriendDo("xiao", "xaoo"));
-		sqlSession.commit();
+	private SqlSession sqlSession;
+
+	private UserMapper mapper;
+
+	public UserService() {
+		sqlSession = MyBatisUtil.getSqlSession();
+		mapper = sqlSession.getMapper(UserMapper.class);
+	}
+//	public static void main(String[] args) {
+//		mapper.addFriend(new UserFriendDo("xiao", "xaoo"));
+//		sqlSession.commit();
+//	}
+
+	public void updateWechatNameByWechatId(String wechatName, String wechatId) {
+		try {
+			mapper.updateWechatNameByWechatId(wechatName, wechatId);
+			sqlSession.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void update(UserDo userDo) {
-		SqlSession sqlSession = MyBatisUtil.getSqlSession();
-		UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-		mapper.updatePasswdByWechatId("12345", "xaohii");
-		sqlSession.commit();
+	public void updatePasswdByWechatId(String passwd, String wechatId) {
+		try {
+			mapper.updatePasswdByWechatId(passwd, wechatId);
+			sqlSession.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
