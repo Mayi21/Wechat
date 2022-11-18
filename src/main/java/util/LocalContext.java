d4a82376033d6217112157295488e5322484c5f4
xaohii <xaohii@163.com>
2022-11-18 21:29:36 +0800

    添加LocalContext

--- src/main/java/util/LocalContext.java
+++ src/main/java/util/LocalContext.java
@@ -6,13 +6,31 @@ public class LocalContext {
 
 	private static ChatCon chatCon;
 
+	private static String wechatId;
+
+	private static String wechatName;
+
 	public static ChatCon getChatCon() {
 		return chatCon;
+	}
+
+	public static String getWechatId() {
+		return wechatId;
+	}
+
+	public static String getWechatName() {
+		return wechatName;
 	}
 
 	public static void setChatCon(ChatCon chatCon) {
 		LocalContext.chatCon = chatCon;
 	}
 
+	public static void setWechatId(String wechatId) {
+		LocalContext.wechatId = wechatId;
+	}
 
+	public static void setWechatName(String wechatName) {
+		LocalContext.wechatName = wechatName;
+	}
 }
