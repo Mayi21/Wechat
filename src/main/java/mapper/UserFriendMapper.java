a2c111afe26902af1acd93d1fbba87999205a950
xaohii <xaohii@163.com>
2022-11-19 22:05:41 +0800

    更新好友列表获取方式

--- src/main/java/mapper/UserFriendMapper.java
+++ src/main/java/mapper/UserFriendMapper.java
@@ -5,10 +5,11 @@ import entity.UserDo;
 import entity.UserFriendDo;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Map;
 import java.util.Set;
 
 public interface UserFriendMapper {
-	public Set<String> getUserFriend(String wechatId);
+	public Map<String, String> getUserFriend(String wechatId);
 
 	public int getFriendStatus(@Param("wechatId") String wechatId, @Param("friendId") String friendId);
 
--- src/main/java/service/UserFriendService.java
+++ src/main/java/service/UserFriendService.java
@@ -2,23 +2,34 @@ package service;
 
 import entity.UserFriendDo;
 import mapper.UserFriendMapper;
+import mapper.UserMapper;
 import org.apache.ibatis.session.SqlSession;
 import util.MyBatisUtil;
 
+import java.util.Map;
+
 public class UserFriendService {
+	private SqlSession sqlSession;
+
+	private UserFriendMapper mapper;
+
+	public UserFriendService() {
+		sqlSession = MyBatisUtil.getSqlSession();
+		mapper = sqlSession.getMapper(UserFriendMapper.class);
+	}
 	public void deleteUserFriend(String wechatId, String friendId) {
-		SqlSession sqlSession = MyBatisUtil.getSqlSession();
-		UserFriendMapper mapper = sqlSession.getMapper(UserFriendMapper.class);
 		mapper.deleteFriend(wechatId);
 		mapper.deleteFriend(friendId);
 		sqlSession.commit();
 	}
 
 	public void addUserFriend(String wechatId, String friendId) {
-		SqlSession sqlSession = MyBatisUtil.getSqlSession();
-		UserFriendMapper mapper = sqlSession.getMapper(UserFriendMapper.class);
 		mapper.addFriend(new UserFriendDo(wechatId, friendId));
 		mapper.addFriend(new UserFriendDo(friendId, wechatId));
 		sqlSession.commit();
+	}
+
+	public Map<String, String> getFriendIdAndName(String id) {
+		return mapper.getUserFriend(id);
 	}
 }
