1a77236bdce51adef0ce88212bd454e409e65a26
xaohii <xaohii@163.com>
2022-11-19 22:05:58 +0800

    更新好友列表获取方式

--- src/main/java/server/UserList.java
+++ src/main/java/server/UserList.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import util.MyBatisUtil;
 
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -15,7 +16,7 @@ public class UserList {
 	public static LinkedList<String> getFriendList(String id){
 		SqlSession sqlSession = MyBatisUtil.getSqlSession();
 		UserFriendMapper mapper = sqlSession.getMapper(UserFriendMapper.class);
-		Set<String> userFriend = mapper.getUserFriend(id);
-		return new LinkedList<>(userFriend);
+		Map<String, String> userFriend = mapper.getUserFriend(id);
+		return new LinkedList<>(userFriend.keySet());
 	}
 }
